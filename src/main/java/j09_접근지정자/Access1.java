338bd4106a23fa49d3339a192c1d7a600a9b3d74
ADbusan <dlsnditk90@naver.com>
2022-11-17 12:54:29 +0900

    자바 접근지정자9

---
+++ src/main/java/j09_접근지정자/Access1.java
@@ -0,0 +1,9 @@
+package j09_접근지정자;
+
+public class Access1 {
+    public String name;
+
+    public void printName(){
+        System.out.println("name: "+name);
+    }
+}
---
+++ src/main/java/j09_접근지정자/캡슐화/StudentMain.java
@@ -0,0 +1,9 @@
+package j09_접근지정자.캡슐화;
+
+public class StudentMain {
+    public static void main(String[] args) {
+        Student student = new Student("박준현", "남");
+        student.printinfo();
+    }
+    }
+
